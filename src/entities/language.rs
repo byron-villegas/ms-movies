5b8bf8f45c7dcb49f3d315a631b493506fab4199
Byron Villegas Moya <byronvillegasm@gmail.com>
2024-11-18 13:03:07 -0300

    Add: add child entities

---
+++ src/entities/language.rs
@@ -0,0 +1,37 @@
+use sea_orm::{ActiveModelBehavior, DeriveEntityModel, DerivePrimaryKey, EntityTrait, EnumIter, PrimaryKeyTrait, Related, RelationDef, RelationTrait};
+
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel)]
+#[sea_orm(table_name = "movie_language")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    #[sea_orm(column_name = "mlg_id")]
+    pub id: i64,
+    #[sea_orm(column_name = "mov_id")]
+    pub movie_id: i64,
+    #[sea_orm(column_name = "mlg_name")]
+    pub name: String
+}
+
+#[derive(Copy, Clone, Debug, EnumIter)]
+pub enum Relation {
+    Movie
+}
+
+impl RelationTrait for Relation {
+    fn def(&self) -> RelationDef {
+        match self {
+            Self::Movie => Entity::belongs_to(super::movie::Entity)
+                .from(Column::MovieId)
+                .to(super::movie::Column::Id)
+                .into(),
+        }
+    }
+}
+
+impl Related<super::movie::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::Movie.def()
+    }
+}
+
+impl ActiveModelBehavior for ActiveModel {}
